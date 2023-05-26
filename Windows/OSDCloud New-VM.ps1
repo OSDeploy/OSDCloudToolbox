04a52e69a3e2420659fe1c80c58d98fe7715d5d0
David Segura <david@segura.org>
2023-05-26 11:54:36 -0500

    Update OSDCloud New-VM.ps1

--- Windows/OSDCloud New-VM.ps1
+++ Windows/OSDCloud New-VM.ps1
@@ -1,4 +1,4 @@
-#Set Parameters
+#Set Hyper-V Configuration
 $vmName = "OSDCloud $(Get-Random)"
 $vmISO = Join-Path $(Get-OSDCloudWorkspace) 'OSDCloud_NoPrompt.iso'
 $vmGeneration = 2
@@ -6,9 +6,11 @@ $vmMemory = 16GB
 $vmProcessorCount = 2
 $vhdSize = 100GB
 
-# Get default VHD path (admin)
+# Get Hyper-V Defaults
 $vmms = Get-WmiObject -Namespace root\virtualization\v2 Msvm_VirtualSystemManagementService
 $vmmsSettings = Get-WmiObject -Namespace root\virtualization\v2 Msvm_VirtualSystemManagementServiceSettingData
+
+# VHD Path
 $vhdPath = Join-Path $vmmsSettings.DefaultVirtualHardDiskPath "$vmName.vhdx"
 
 # Create VM
@@ -17,34 +19,32 @@ $vm | Set-VMProcessor -Count $vmProcessorCount -Verbose
 $vm | Get-VMIntegrationService -Name "Guest Service Interface" | Enable-VMIntegrationService -Verbose
 $vm | Set-VMMemory -DynamicMemoryEnabled $false -Verbose
 
-
-$vmHardDiskDrive = $vm | Get-VMHardDiskDrive
-$vmNetworkAdapter = $vm | Get-VMNetworkAdapter
-
-# Set ISO
+# DVD and ISO
 $vmDVD = $vm | Add-VMDvdDrive -Path $vmISO -Passthru
 $vm | Set-VMFirmware -FirstBootDevice $vmDVD
+
+# Boot Order
+$vmHardDiskDrive = $vm | Get-VMHardDiskDrive
+$vmNetworkAdapter = $vm | Get-VMNetworkAdapter
 $vm | Set-VMFirmware -BootOrder $vmDVD, $vmHardDiskDrive, $vmNetworkAdapter
 
-# Set Secure Boot
-#$vm | Set-VMFirmware -SecureBootTemplateId ([guid]'272e7447-90a4-4563-a4b9-8e4ab00526ce')
+# Secure Boot
 $vm | Set-VMFirmware -EnableSecureBoot On
 
-# Enable TPM
+# TPM
 if ((Get-TPM).TpmPresent -eq $true -and (Get-TPM).TpmReady -eq $true) {
     $vm | Set-VMSecurity -VirtualizationBasedSecurityOptOut:$false
     $vm | Set-VMKeyProtector -NewLocalKeyProtector
     $vm | Enable-VMTPM
 }
 
-# This breaks Set-DisRes in WinPE
-#Set-VMVideo -VMName $vmName -ResolutionType Single -HorizontalResolution 1280 -VerticalResolution 800
-
-# Checkpoint and Snapshot
+# Checkpoint
 $vm | Set-VM -AutomaticCheckpointsEnabled $false
 $vm | Set-VM -AutomaticStartAction Nothing
 $vm | Set-VM -AutomaticStartDelay 3
 $vm | Set-VM -AutomaticStopAction Shutdown
+
+# Snapshot
 $vm | Checkpoint-VM -SnapshotName 'New-VM'
 
 # Start VM
