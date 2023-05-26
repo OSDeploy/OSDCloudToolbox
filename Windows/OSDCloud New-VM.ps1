0a3d4008ea1ffc9e225cfb79690d410a1abafc3a
David Segura <david@segura.org>
2023-05-26 12:14:58 -0500

    Update OSDCloud New-VM.ps1

--- Windows/OSDCloud New-VM.ps1
+++ Windows/OSDCloud New-VM.ps1
@@ -1,6 +1,6 @@
 #Set Hyper-V Configuration
 $vmName = "OSDCloud $(Get-Random)"
-$vmISO = Join-Path $(Get-OSDCloudWorkspace) 'OSDCloud_NoPrompt.iso'
+$vmIso = Join-Path $(Get-OSDCloudWorkspace) 'OSDCloud_NoPrompt.iso'
 $vmGeneration = 2
 $vmMemory = 16GB
 $vmProcessorCount = 2
@@ -10,47 +10,45 @@ $vhdSize = 100GB
 $vmms = Get-WmiObject -Namespace root\virtualization\v2 Msvm_VirtualSystemManagementService
 $vmmsSettings = Get-WmiObject -Namespace root\virtualization\v2 Msvm_VirtualSystemManagementServiceSettingData
 
-# VHD Path
+# Create VHDX VM DVD ISO
 $vhdPath = Join-Path $vmmsSettings.DefaultVirtualHardDiskPath "$vmName.vhdx"
-
-# Create VM
-$vm = New-VM -Name $vmName -Generation $vmGeneration -MemoryStartupBytes $vmMemory -NewVHDPath $vhdPath -NewVHDSizeBytes $vhdSize -Verbose -SwitchName 'Default Switch'
-$vm | Set-VMProcessor -Count $vmProcessorCount -Verbose
-$vm | Get-VMIntegrationService -Name "Guest Service Interface" | Enable-VMIntegrationService -Verbose
-$vm | Set-VMMemory -DynamicMemoryEnabled $false -Verbose
-
-# DVD and ISO
-$vmDVD = $vm | Add-VMDvdDrive -Path $vmISO -Passthru
-$vm | Set-VMFirmware -FirstBootDevice $vmDVD
-
-# Boot Order
+$vm = New-VM -Name $vmName -Generation $vmGeneration -MemoryStartupBytes $vmMemory -NewVHDPath $vhdPath -NewVHDSizeBytes $vhdSize -SwitchName 'Default Switch' -Verbose
+$vmDvd = $vm | Add-VMDvdDrive -Path $vmIso -Passthru -Verbose
+$vm | Set-VMFirmware -FirstBootDevice $vmDvd -Verbose
 $vmHardDiskDrive = $vm | Get-VMHardDiskDrive
 $vmNetworkAdapter = $vm | Get-VMNetworkAdapter
-$vm | Set-VMFirmware -BootOrder $vmDVD, $vmHardDiskDrive, $vmNetworkAdapter
 
-# Secure Boot
-$vm | Set-VMFirmware -EnableSecureBoot On
+# Firmware
+$vm | Set-VMFirmware -BootOrder $vmDvd, $vmHardDiskDrive, $vmNetworkAdapter -Verbose
 
-# TPM
+# Security
+$vm | Set-VMFirmware -EnableSecureBoot On -Verbose
 if ((Get-TPM).TpmPresent -eq $true -and (Get-TPM).TpmReady -eq $true) {
-    $vm | Set-VMSecurity -VirtualizationBasedSecurityOptOut:$false
-    $vm | Set-VMKeyProtector -NewLocalKeyProtector
-    $vm | Enable-VMTPM
+    $vm | Set-VMSecurity -VirtualizationBasedSecurityOptOut:$false -Verbose
+    $vm | Set-VMKeyProtector -NewLocalKeyProtector -Verbose
+    $vm | Enable-VMTPM -Verbose
 }
 
-# Checkpoint
-$vm | Set-VM -AutomaticCheckpointsEnabled $false
-$vm | Set-VM -AutomaticStartAction Nothing
-$vm | Set-VM -AutomaticStartDelay 3
-$vm | Set-VM -AutomaticStopAction Shutdown
+# Memory
+$vm | Set-VMMemory -DynamicMemoryEnabled $false -Verbose
 
-# Snapshot
-$vm | Checkpoint-VM -SnapshotName 'New-VM'
+# Processor
+$vm | Set-VMProcessor -Count $vmProcessorCount -Verbose
+
+# Integration Services
+$vm | Get-VMIntegrationService -Name "Guest Service Interface" | Enable-VMIntegrationService -Verbose
+
+# Checkpoints
+$vm | Set-VM -AutomaticCheckpointsEnabled $false -Verbose
+
+# Automatic Start Action
+$vm | Set-VM -AutomaticStartAction Nothing -AutomaticStartDelay 3 -Verbose
+
+# Automatic Stop Action
+$vm | Set-VM -AutomaticStopAction Shutdown -Verbose
 
 # Start VM
+$vm | Checkpoint-VM -SnapshotName 'New-VM' -Verbose
 vmconnect.exe $env:ComputerName $vmName
 Start-Sleep -Seconds 3
-$vm | Start-VM
-
-# Wait for Windows
-#$vm | Wait-VM -For Heartbeat
+$vm | Start-VM -Verbose
